61671b3edd9f40411d7d3bb844ad56b66c4aea6c
InigoCL <inigocl21@gmail.com>
2024-10-19 18:18:43 +0200

    Ejercicio base hecho. Falta JavaDoc y mirar optimizacion/organizacion

--- tarea_4/src/tarea_4/Main.java
+++ tarea_4/src/tarea_4/Main.java
@@ -1,10 +1,55 @@
 package tarea_4;
 
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int NUMERO_ALUMNOS = 5;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime el nombre del fichero en el que quieres guardar la información");
+		String nombre = sc.nextLine();
 
+		System.out.println("Dime la ruta donde quieres guardar el archivo");
+		String ruta = sc.nextLine();
+
+		if (ruta.equalsIgnoreCase("")) {
+			ruta = "src";
+		}
+
+		DataOutputStream dos = null;
+		try {
+			dos = new DataOutputStream(new FileOutputStream(ruta + "\\" + nombre + ".bin"));
+			for (int i = 0; i < NUMERO_ALUMNOS; i++) {
+				Alumno a = new Alumno();
+				a.pedirDatos(sc, i);
+				dos.writeInt(a.getDni());
+				dos.writeUTF(a.getNombre());
+				dos.writeUTF(a.getApellidos());
+				dos.writeChar(a.getGenero());
+				dos.writeLong(a.getFechaNacimiento().getTime());
+				dos.writeUTF(a.getCiclo());
+				dos.writeUTF(a.getCurso());
+				dos.writeUTF(a.getGrupo());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (dos != null) {
+				try {
+					dos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			sc.close();
+		}
 	}
 
 }
